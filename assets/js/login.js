bc1949377080f2e8d4e6dfafc8af22e16bad7a91
icat <G_tingting3@163.com>
2021-07-09 18:10:16 +0800

    完成了主页功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,17 +1,68 @@
 //获取用户的基本信息
+
+
+$(function () {
+    getUserInfo()
+
+})
+
+//获取用户的基本信息
 function getUserInfo() {
     $.ajax({
         type: "GET",
         url: '/my/userinfo',
-        headers: {
-            Authorization: localStorage.getItem("token") || ''
-        },
+        // headers: {
+        //     Authorization: localStorage.getItem("token") || ''
+        // },
         success: function (res) {
-            console.log(res);
+            //console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg("获取用户信息失败")
+            }
+            //调用renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        },
+        complete: function (res) {
+            // console.log(res);
+            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                localStorage.removeItem("token")
+                location.href = "/index.html"
 
+            }
         }
-
+        // complete: function (res) {
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         localStorage.removeItem("token")
+        //         location.href = "/login.html"
+        //     }
+        // }
     })
 }
-getUserInfo()
+//渲染用户头像
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $(".welcome").html("欢迎" + name)
+    if (user.user_pic !== null) {
+        $(".layui-nav-img").attr("src", user.user_pic).show()
+        $(".text-avatar").hide()
+    } else {
+        $(".layui-nav-img").hide()
+        var first_word = name[0].toUpperCase()
+        $(".text-avatar").html(first_word).show()
+    }
+}
+//实现退出功能
+$("#btnLoginout").on("click", function () {
+    layer.confirm('确认退出登陆吗?', { icon: 3, title: '提示' }, function (index) {
+        localStorage.removeItem('token')
+        location.href = "/index.html"
+
+        layer.close(index);
+    });
+
+})
+
+
+
+
 
